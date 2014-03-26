48aab75fad64a1506e7ba254c18c7146ea30f071
zzzaaa12 <zzzaaa12@gmail.com>
2014-03-26 14:57:20 +0800

    add content for setting.sh

---
+++ bash/setting.sh
@@ -0,0 +1,49 @@
+# PS1 setting with color 
+PS1=' \[\033[01;32m\]\W\[\033[00m\]\$ '
+
+# PS1 setting without color 
+##PS1=' \W\$ '
+
+# alias for commands with color
+alias diff='colordiff'
+alias grep='grep --color=auto --exclude-dir=.svn --exclude-dir=CVS'
+alias findx='find . -name'
+
+# color for ls command
+LS_COLORS="rs=0:di=01;36:ln=02;33:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lz=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.axv=01;35:*.anx=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.axa=00;36:*.oga=00;36:*.spx=00;36:*.xspf=00;36:"
+
+# alias for jobs/fg command
+alias m='jobs'
+alias 1='fg 1'
+alias 2='fg 2'
+alias 3='fg 3'
+alias 4='fg 4'
+alias 5='fg 5'
+alias 6='fg 6'
+alias 7='fg 7'
+alias 8='fg 8'
+alias 9='fg 9'
+alias 10='fg 10'
+alias 11='fg 11'
+alias 12='fg 12'
+alias 13='fg 13'
+alias 14='fg 14'
+alias 15='fg 15'
+alias 16='fg 16'
+alias 17='fg 17'
+alias 18='fg 18'
+alias 19='fg 19'
+alias 19='fg 19'
+
+# alias for cd commands
+alias cd1='cd ..'
+alias cd2='cd ../../'
+alias cd3='cd ../../../'
+alias cd4='cd ../../../../'
+alias cd5='cd ../../../../../'
+alias cd6='cd ../../../../../../'
+alias cd7='cd ../../../../../../../'
+alias cd8='cd ../../../../../../../../'
+alias cd9='cd ../../../../../../../../../'
+alias cd10='cd ../../../../../../../../../../'
+
