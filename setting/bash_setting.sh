3a63b39265862f71e46ff93e65f1b1cb28138fee
Alex.Lin <Alex.Lin@comtrend.com>
2019-03-15 10:34:29 +0800

    update bash_setting.sh and vimrc

--- setting/bash_setting.sh
+++ setting/bash_setting.sh
@@ -1,8 +1,9 @@
 # PS1 setting with color 
-PS1=' \[\033[01;32m\]\W\[\033[00m\] \$ '
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+export PS1=" \[\033[01;32m\]\W\[\033[31m\]\$(parse_git_branch)\[\033[00m\] $ "
 
-# PS1 setting without color 
-##PS1=' \W\$ '
 
 # alias for commands with color
 alias diff='colordiff'
