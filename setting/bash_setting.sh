5c3c97f7856b4dc27df626b1700f770b3c947cbe
zzzaaa12 <zzzaaa12@users.noreply.github.com>
2023-08-14 17:12:13 +0800

    Update bash_setting.sh

--- setting/bash_setting.sh
+++ setting/bash_setting.sh
@@ -2,7 +2,12 @@
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
-export PS1=" \[\033[01;32m\]\W\[\033[31m\]\$(parse_git_branch)\[\033[00m\] $ "
+
+if [ "$(hostname|grep '\[')" != "" ]; then
+    export PS1=" \u@\h \[\033[01;32m\]\W\[\033[31m\]\$(parse_git_branch)\[\033[00m\] $ "
+else
+    export PS1=" \[\033[01;32m\]\W\[\033[31m\]\$(parse_git_branch)\[\033[00m\] $ "
+fi
 
 LANG=C
 
